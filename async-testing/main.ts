34091b816cbce9e7f68fe3cc7d15f7382e6b2577
Ondřej Netočný <ondra.netocny@hotmail.com>
2022-12-22 08:21:45 +0000

    Rename

--- async-testing/main.ts
+++ async-testing/main.ts
@@ -3,14 +3,14 @@ function foo(val: number): Promise<string>
     return new Promise<string>((resolve, reject) => val % 2 === 0 ? resolve(`success ${val}`) : reject(`failure ${val}`));
 }
   
-async function bar(val: number): Promise<void>
+async function bar(sleep: number): Promise<void>
 {
     try
     {
-        console.log(`Waiting ${val} seconds...`);
-        await new Promise(r => setTimeout(r, val * 1000));
-        console.log(`Waited ${val} seconds.`);
-        const result = await foo(val);
+        console.log(`Waiting ${sleep} seconds...`);
+        await new Promise(r => setTimeout(r, sleep * 1000));
+        console.log(`Waited ${sleep} seconds.`);
+        const result = await foo(sleep);
         console.log('Result: ' + result);
     }
     catch (error)

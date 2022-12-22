c5eebd0c3b1e8cd9a2f2d47b6b0b3692f66a2d91
Ondřej Netočný <ondra.netocny@hotmail.com>
2022-12-22 07:37:42 +0000

    Formatting

--- async-testing/main.ts
+++ async-testing/main.ts
@@ -1,16 +1,21 @@
-function foo(val: number): Promise<string> {
+function foo(val: number): Promise<string>
+{
     return new Promise<string>((resolve, reject) => val % 2 === 0 ? resolve(`success ${val}`) : reject(`failure ${val}`));
-  }
+}
   
-  async function bar(val: number): Promise<void> {
-    try {
-      var result = await foo(val);
-      console.log('Result: ' + result);
-    } catch (error) {
-      console.log('Error: ' + error);
+async function bar(val: number): Promise<void>
+{
+    try
+    {
+        const result = await foo(val);
+        console.log('Result: ' + result);
     }
-  }
+    catch (error)
+    {
+        console.log('Error: ' + error);
+    }
+}
   
-  bar(1).then(() => console.log('Done 1'));
-  bar(2).then(() => console.log('Done 2'));
+bar(1).then(() => console.log('Done 1'));
+bar(2).then(() => console.log('Done 2'));
   
